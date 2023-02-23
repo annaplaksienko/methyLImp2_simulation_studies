8e87790a54df8c3f32c38792db1b8031d5bd1bfa
annaplaksienko <anna.plaksienko@gssi.it>
2023-02-23 17:54:02 +0100

    Annotation construction

---
+++ R/Annotation_from_csv_450K.R
@@ -0,0 +1,26 @@
+#Data matches each probe of Illumina 450K array to a chromosome. 
+#File is from https://support.illumina.com/array/array_kits/infinium_humanmethylation450_beadchip_kit/downloads.html -> 
+#Infinium HumanMethylation450K v1.2 Product Files -> 
+#HumanMethylation450 v1.2 Manifest File (CSV Format).
+#Too big to put on Github
+
+data <- read.table("humanmethylation450_15017482_v1-2.csv", 
+                   sep = ",", quote = "", col.names = c(1:33), fill = TRUE)
+#move colnames from cells to colnames
+colnames(data) <- data[8, ]
+#drop the heading
+data <- data[-c(1:8), ]
+#keep only probes and chromosomes
+data <- data.frame(cpg = data$IlmnID, chr = data$CHR)
+dim(data)
+
+unique(data$chr)
+#drop probes without chromosomes assigned
+data <- data[data$chr != "", ]
+#drop sex chromosomes (since we won't be doing imputations there)
+data <- data[data$chr != "X", ]
+data <- data[data$chr != "Y", ]
+dim(data)
+
+anno450K <- data
+save(list = c("anno450K"), file = "anno450K.RData")
---
+++ R/Annotation_from_csv_EPIC.R
@@ -0,0 +1,27 @@
+#Data matches each probe of Illumina EPIC array to a chromosome. 
+#File is from https://support.illumina.com/array/array_kits/infinium-methylationepic-beadchip-kit/downloads.html -> 
+#Infinium MethylationEPIC v1.0 Product Files (Legacy BeadChip) -> 
+#Infinium MethylationEPIC v1.0 B5 Manifest File (CSV Format).
+#File is too big to put on Github
+
+data <- read.table("infinium-methylationepic-v-1-0-b5-manifest-file.csv", 
+                   sep = ",", quote = "", col.names = c(1:52), fill = TRUE)
+
+#move colnames from cells to colnames
+colnames(data) <- data[8, ]
+#drop the heading
+data <- data[-c(1:8), ]
+#keep only probes and chromosomes
+data <- data.frame(cpg = data$IlmnID, chr = data$CHR)
+dim(data)
+
+unique(data$chr)
+#drop probes without chromosomes assigned
+data <- data[data$chr != "", ]
+#drop sex chromosomes (since we won't be doing imputations there)
+data <- data[data$chr != "X", ]
+data <- data[data$chr != "Y", ]
+dim(data)
+
+annoEPIC <- data
+save(list = c("annoEPIC"), file = "annoEPIC.RData")
